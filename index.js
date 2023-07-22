0ad99636953d596e1874e9a2c8b1d488d98b30bd
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-07-22 16:01:06 -0300

    agregue el puerto a utilizar

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import express from 'express';
-console.log("esta es una prueba")
+
+//configuro el puerto-creo la instancia
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), ()=>{
+    console.log('Puerto:'+ app.get("port"));
+})

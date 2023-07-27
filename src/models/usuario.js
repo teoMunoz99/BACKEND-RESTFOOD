773400f4d0b57c8bb801075e8fd120bafc1ff634
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-27 13:43:10 -0300

    Cambio de modelo en el usuario y sus propiedades

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,80 +1,79 @@
 import mongoose, { Schema } from "mongoose";
 
 const pedidoSchema = new Schema({
-    domicilio: {
-        type: String,
-        required: true
-    },
-    total: {
-        type: String,
-        required: true
-    },
-    nombreProducto: {
-        type: [String],
-        required: true
-    },
-    estado: {
-        type: String,
-        required: true
-    },
-    fecha: {
-        type: String,
-        required: true
-    },
-    envio: {
-        type: String,
-        required: true
-    }
+  domicilio: {
+    type: String,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  nombresProductos: {
+    type: [String],
+    required: true,
+  },
+  estado: {
+    type: String,
+    required: true,
+  },
+  fecha: {
+    type: String,
+    required: true,
+  },
+  envio: {
+    type: [Number],
+    required: true,
+  },
 });
 
 const usuarioSchema = new Schema({
-    nombreUsuario: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        maxlength: 100,
-        match: /^\S+@\S+\.\S+$/
-    },
-    contrasenia: {
-        type: String,
-        required: true,
-        minlength: 6
-    },
-    imagen: {
-        type: String,
-        required: true
-    },
-    estado: {
-        type: String,
-        enum: ['activo', 'inactivo'], // Estado debe ser 'activo' o 'inactivo'
-        required: true
-    },
-    rol: {
-        type: String,
-        enum: ['usuario', 'administrador'], // Rol debe ser 'usuario' o 'administrador'
-        required: true
-    },
-    pedido: {
-        type: [pedidoSchema],
-        default: [] 
-    },
-    favoritos: {
-        type: [String],
-        default: []
-    },
-    carrito: {
-        type: [String],
-        default: []
-    }
+  nombre: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 100,
+    match: /^\S+@\S+\.\S+$/,
+  },
+  contrasenia: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  estado: {
+    type: String,
+    enum: "activo",
+    required: true,
+  },
+  rol: {
+    type: String,
+    enum: "usuario",
+    required: true,
+  },
+  pedido: {
+    type: [pedidoSchema],
+    default: [],
+  },
+  favoritos: {
+    type: [Number],
+    default: [],
+  },
+  carrito: {
+    type: [String],
+    default: [],
+  },
 });
 
-
-const Usuario = mongoose.model('usuario', usuarioSchema)
+const Usuario = mongoose.model("usuario", usuarioSchema);
 
 export default Usuario;

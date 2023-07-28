25b14b069454e311b19eadcdda1baa22cbb928b4
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-28 13:53:02 -0300

    Se corrigió length de la descripción

--- src/models/plato.js
+++ src/models/plato.js
@@ -1,39 +1,45 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model } from "mongoose";
 
 const platoSchema = new Schema({
-    nombre: {
-        type: String,
-        required: true, 
-        minlength: 3,   
-        maxlength: 100  
-    },
-    imagen: {
-        type: String,
-        required: true
-    },
-    precio: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    descripcion: {
-        type: String,
-        minlength: 3,
-        maxlength: 200,
-        required: true
-    },
-    stock: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    categoria: {
-        type: String,
-        required: true,
-        enum: ['entradas', 'bebidas', 'postres', 'bebidasAlcoholicas', 'comidasVeganas']
-    }
+  nombre: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 100,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  descripcion: {
+    type: String,
+    minlength: 3,
+    maxlength: 800,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: [
+      "entradas",
+      "bebidas",
+      "postres",
+      "bebidasAlcoholicas",
+      "comidasVeganas",
+    ],
+  },
 });
 
-const Plato = model('plato', platoSchema);
+const Plato = model("plato", platoSchema);
 
 export default Plato;

22be4dec87330c1b6153ce151eef8151f7137ef7
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-28 15:00:27 -0300

    Se corrigieron las validaciones para crear el producto

--- src/helpers/validarPlato.js
+++ src/helpers/validarPlato.js
@@ -4,25 +4,25 @@ import resultadoValidacion from "./resultadoValidacion";
 const validarPlato = [
   check("nombre")
     .notEmpty()
-    .withMessage("El nombre del plato es un dato obligatorio")
+    .withMessage("El nombre del plato es un dato obligatorio.")
     .isLength({ min: 3, max: 100 })
-    .withMessage("El nombre del plato debe tener entre 3 y 100 caracteres"),
+    .withMessage("El nombre del plato debe tener entre 3 y 100 caracteres."),
   check("precio")
     .notEmpty()
-    .withMessage("El precio es un dato obligatorio")
+    .withMessage("El precio es un dato obligatorio.")
     .isNumeric()
-    .withMessage("El precio debe ser un numero")
+    .withMessage("El precio debe ser un número.")
     .custom((value) => {
       if (value >= 0) {
         return true;
       } else {
-        throw new Error("El precio debe ser mayor o igual a 0");
+        throw new Error("El precio debe ser mayor o igual a 0.");
       }
     }),
-  check("imagen").notEmpty().withMessage("Es necesario agregar una imagen"),
+  check("imagen").notEmpty().withMessage("Es necesario agregar una imágen."),
   check("categoria")
     .notEmpty()
-    .withMessage("La categoria es un dato obligatorio")
+    .withMessage("La categoria es un dato obligatorio.")
     .isIn([
       "entradas",
       "bebidas",
@@ -30,22 +30,22 @@ const validarPlato = [
       "bebidasAlcoholicas",
       "comidasVeganas",
     ])
-    .withMessage("Debe agregar una categoria valida"),
+    .withMessage("Debe agregar una categoria válida."),
   check("descripcion")
     .notEmpty()
-    .withMessage("Es necesario agregar una descripcion")
+    .withMessage("Es necesario agregar una descripción.")
     .isLength({ min: 3, max: 800 })
-    .withMessage("La descripcion debe tener entre 3 y 800 caracteres"),
+    .withMessage("La descripción debe tener entre 3 y 800 caracteres."),
   check("stock")
     .notEmpty()
-    .withMessage("El stock es un dato obligatorio")
+    .withMessage("El stock es un dato obligatorio.")
     .isNumeric()
-    .withMessage("El stock debe ser un numero")
+    .withMessage("El stock debe ser un número.")
     .custom((value) => {
       if (value >= 0) {
         return true;
       } else {
-        throw new Error("El stock debe ser mayor o igual a 0");
+        throw new Error("El stock debe ser mayor o igual a 0.");
       }
     }),
   (req, res, next) => {
